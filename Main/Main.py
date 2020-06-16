5d2114825d700dc8b881cea62b5152e0591816bb
AWPryor <55792412+AWPryor@users.noreply.github.com>
2020-06-16 21:46:08 +0100

    updates

--- Main/Main.py
+++ Main/Main.py
@@ -1,7 +1,20 @@
 #Tinder bot account creator
 
 
-    #TODO: Run MD5sig.def
-    #        run vpncon.py
-    #        run vpndis.py
-    #
+def intro():
+    print("Hello, Welcome to the tinder account creation tool");
+    print("Press 1 to change image signitures");
+    print("Press 2 to connect to Nord VPN");
+    print("Press 3 to disconnect to Nord VPN");
+
+    choise = input("Select a number")
+    if (choise == 1):
+            import MD5Sig;
+
+    elif (choise ==2):
+        import vpnCon;
+    elif (choise ==3):
+        import vpnDis;
+
+
+intro()
--- Main/vpnDis.py
+++ Main/vpnDis.py
@@ -1,2 +1,2 @@
 import subprocess
-subprocess.call([r'C:\Users\User\Documents\GitHub\binderbot\Bat\Disconect.bat']);
+subprocess.call([r'C:\Users\User\Documents\GitHub\binderbot\Main\Bat\Disconect.bat']);
