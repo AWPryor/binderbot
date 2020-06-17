d10dae1f0da6ccc9a36f1c4a67f090027aa82d27
Hacksaw <callumwaite203@gmail.com>
2020-06-17 19:48:54 +0100

    Removed Head shit
    see title

--- Main/Bat/ClearBrowser.bat
+++ Main/Bat/ClearBrowser.bat
@@ -1,7 +1,3 @@
 cd "C:\Program Files\CCleaner"
-<<<<<<< HEAD
-=======
-C:\Program Files\CCleaner
->>>>>>> 57bc458e78660259ce7314dd67ce1c4373eb9442
 CCleaner.exe /AUTO
-echo "CCleaner should have run."
+echo "Custom broswer settings cleared!"
