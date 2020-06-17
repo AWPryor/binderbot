fc81bdf97eac6f06edaace95b2d102705ebdb4ce
AWPryor <55792412+AWPryor@users.noreply.github.com>
2020-06-17 16:59:49 +0100

    Update phoneapi.py

--- Main/phoneapi.py
+++ Main/phoneapi.py
@@ -1,43 +1,83 @@
 import requests
-phoneid = [];
+apikey = 'eefdddb60352ece3762c37AcA4efccA2';
 
 
 def ask():
     print("Press 1 to check balance");
-    print("Press 2 to check order gmail ukrane phone")
-    print("Press 3 to check order")
+    print("Press 2 to order phones")
     choise = input("Hello, Please select a service")
 
     if (choise == '1') :
         accBalance();
         ask();
+
     elif (choise =='2'):
-        savePhone();
-        ask();
-    elif (choise =='3'):
-        print(savePhone);
-        checkPhone(phoneid);
+        alwaystrue = True;
+        print('Press 1 for Ukrane')
+        print('Press 2 for England')
+        print('Press 3 for Portugal')
+        print('Press 4 for Sweden')
+        country = input('Select a number: ')
+        if (country == '1'):
+            countryId = '1';
+        elif (country == '2'):
+            countryId = '16';
+        elif (country == '3'):
+            countryId = '117';
+        elif (country == '4'):
+            countryId = '46';
+
+        print('Press 1 for Google')
+        print('Press 2 for tinder')
+        service = input('Select an option: ')
+        if (service == '1'):
+            serviceId = 'go'
+        elif (service == '2'):
+            serviceId='oi'
+        print(serviceId)
+        print(countryId)
+        phoneid=buyPhone(serviceId, countryId);
+        while alwaystrue:
+            print('Press 1 to check status')
+            print('Press 2 to check phone number')
+            print('Press 3 to exit - Note if you had an error please exit and reorder')
+            b = input("Select your option: ")
+
+            if (b == '1'):
+                print(phoneid[1])
+                response2 = requests.post('https://sms-activate.ru/stubs/handler_api.php?api_key='+apikey+'&action=getStatus&id='+phoneid[1]);
+                Responce2 = response2.text;
+                print(Responce2);
+            elif (b == '2'):
+                print(phoneid[2])
+            elif (b == '3'):
+                break;
 
         ask();
-def savePhone():
-    phoneid = buyPhone();
+
+
+    elif (choise =='3'):
+        checkPhone(phoneid);
+        ask();
+
 
 def accBalance():
-    response = requests.post('https://sms-activate.ru/stubs/handler_api.php?api_key=eefdddb60352ece3762c37AcA4efccA2&action=getBalance')
+    response = requests.post('https://sms-activate.ru/stubs/handler_api.php?api_key='+apikey+'&action=getBalance')
     Responce = response.text
     print(Responce)
 
-def buyPhone():
-    response1 = requests.post('https://sms-activate.ru/stubs/handler_api.php?api_key=eefdddb60352ece3762c37AcA4efccA2&action=getNumber&service=oi$&ref=745234$ref&country=1');
+def buyPhone(serviceId, countryId):
+    response1 = requests.post('https://sms-activate.ru/stubs/handler_api.php?api_key='+apikey+'&action=getNumber&service='+serviceId+'$&ref=745234$ref&country='+countryId);
     Responce1 = response1.text;
     print(Responce1);
     return Responce1.split(":");
 
 def checkPhone(phoneid):
         print(phoneid)
-        response2 = requests.post('https://sms-activate.ru/stubs/handler_api.php?api_key=eefdddb60352ece3762c37AcA4efccA2&action=getStatus&id='+phoneid[1] + '275173238');
+        response2 = requests.post('https://sms-activate.ru/stubs/handler_api.php?api_key=eefdddb60352ece3762c37AcA4efccA2&action=getStatus&id='+phoneid[1]+'275173238');
         Responce2 = response2.text;
         print(Responce2);
 
 
 ask();
+1
